7f12ccd656327a1ed5881e4ffa775324abccfa52
Dileep T Paul <dileeptpaul2019@gmail.com>
2020-05-18 13:07:46 +0530

    Update requestmapping for homecontroller

--- backend/src/main/java/com/pm/controller/HomeController.java
+++ backend/src/main/java/com/pm/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"", "/", "adduser", "addtask", "addproject", "viewtask"})
     public String home() {
         return "forward:/index.html";
     }
